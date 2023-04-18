42b865489cdcd71838eeffab57e3ff5bf00bc2f3
Иван <i.tarchevsky@gmail.com>
2023-04-18 15:14:53 +0300

    init: project

---
+++ src/blocks/modules/merch/merch.js
@@ -0,0 +1,13 @@
+modules.define('merch', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
---
+++ src/blocks/modules/trainings/trainings.js
@@ -0,0 +1,13 @@
+modules.define('trainings', ['i-bem-dom'], function(provide, bemDom) {
+
+provide(bemDom.declBlock(this.name, {
+    onSetMod: {
+        js: {
+            inited: function() {
+                
+            }
+        }
+    }
+}));
+
+});
